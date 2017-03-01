26cc41300fa66dbed79262c0e96efdd720083c2b
frodank <frodank@gmail.com>
2017-03-01 17:41:29 +0100

    Made TestChoice reusable for all singlewinner-electionsystems

--- src/test/java/temp/frodank/electionsystem/singlewinnermethods/FirstPastThePostTest.java
+++ src/test/java/temp/frodank/electionsystem/singlewinnermethods/FirstPastThePostTest.java
@@ -23,49 +23,6 @@ import temp.frodank.electionsystem.SingleChoiceTieBreaker;
  * @author frodank
  */
 public class FirstPastThePostTest {
-    class TestChoice implements Choice<TestChoice> {
-        private String name;
-
-        public TestChoice(String name) {
-            this.name = name;
-        }
-        @Override
-        public String getName() {
-            return name;
-        }
-        @Override
-        public int compareTo(TestChoice o) {
-            return this.name.compareTo(o.name);
-        }
-
-        @Override
-        public int hashCode() {
-            int hash = 3;
-            hash = 29 * hash + Objects.hashCode(this.name);
-            return hash;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final TestChoice other = (TestChoice) obj;
-            if (!Objects.equals(this.name, other.name)) {
-                return false;
-            }
-            return true;
-        }
-
-        @Override
-        public String toString() {
-            return name;
-        }
-    }
-    
     @Test
     public void test() {
         FirstPastThePost fptp = new FirstPastThePost();
